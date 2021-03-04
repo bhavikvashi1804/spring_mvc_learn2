94eb535f1d678343d62c80b1b2bb72ebd03a93dd
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-03-04 20:22:23 +0530

    Initial commit

---
+++ src/main/java/com/bhavik/springmvc/SpringMvcLearnApplication.java
@@ -0,0 +1,13 @@
+package com.bhavik.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhavik/springmvc/SpringMvcLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhavik.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
