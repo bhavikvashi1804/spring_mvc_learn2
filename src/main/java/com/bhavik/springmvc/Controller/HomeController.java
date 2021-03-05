7d5dd9178dc4f799846e5198350c91f66f55add1
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-03-05 08:11:46 +0530

    User Form: Simple calculator add two number

--- src/main/java/com/bhavik/springmvc/Controller/HomeController.java
+++ src/main/java/com/bhavik/springmvc/Controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.bhavik.springmvc.Controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,6 +12,22 @@ class HomeController {
 	@GetMapping("/")
 	public String showWelcomePage() {
 		return "Welcome.jsp";
+	}
+	
+	@GetMapping("/calculator")
+	public String showCalPage() {
+		return "cal.jsp";
+	}
+	
+	@GetMapping("/result")
+	public String showResultPage(HttpServletRequest req) {
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		int sum = i + j;
+		
+		HttpSession session = req.getSession();
+		session.setAttribute("sum", sum);
+		return "result.jsp";
 	}
 
 }
