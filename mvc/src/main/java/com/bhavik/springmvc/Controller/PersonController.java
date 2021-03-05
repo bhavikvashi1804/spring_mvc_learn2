0a350e6ff75b2a0d122855e2b95b4be57048143a
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-03-05 12:16:46 +0530

    problem solve by isELIgnored

--- mvc/src/main/java/com/bhavik/springmvc/Controller/PersonController.java
+++ mvc/src/main/java/com/bhavik/springmvc/Controller/PersonController.java
@@ -1,11 +1,14 @@
 package com.bhavik.springmvc.Controller;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.bhavik.springmvc.model.Person;
 
@@ -24,9 +27,18 @@ public class PersonController {
 		return "PersonForm";
 	}
 
-	@GetMapping("/submitPerson")
+	@PostMapping("/submitPerson")
 	public String showPersonResult(@ModelAttribute("person") Person p) {
-		// We do not have to pass the ModelAttribute to next page, it will automatically do it		
+		// We do not have to pass the ModelAttribute to next page, it will automatically do it	
+		System.out.println(p.getId());
 		return "personresult";
+	}
+	
+	
+	@GetMapping("/getPersons")
+	public String displayPersons(ModelMap m) {
+		List<Person> myList = Arrays.asList(new Person(101,"Bhavik",22), new Person(102,"Raj",18));
+		m.addAttribute("persons", myList);
+		return "list-person";
 	}
 }
--- mvc/src/main/webapp/WEB-INF/views/PersonForm.jsp
+++ mvc/src/main/webapp/WEB-INF/views/PersonForm.jsp
@@ -7,7 +7,7 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<form action="/submitPerson">
+	<form action="/submitPerson" method="post">
 		<table>
 			<tr>
 				<td>Person ID</td>
