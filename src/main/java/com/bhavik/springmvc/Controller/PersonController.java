0d9fed15b78ffaef6c7dcae000a48d3e00ddf386
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-03-05 10:58:55 +0530

    @ModelAttribute at the method level to pass the data to all C and C will pass it to V

--- src/main/java/com/bhavik/springmvc/Controller/PersonController.java
+++ src/main/java/com/bhavik/springmvc/Controller/PersonController.java
@@ -1,6 +1,9 @@
 package com.bhavik.springmvc.Controller;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
@@ -9,6 +12,13 @@ import com.bhavik.springmvc.model.Person;
 @Controller
 public class PersonController {
 
+	
+	@ModelAttribute
+	public void setDate(ModelMap m) {
+		LocalDateTime ldt = LocalDateTime.now();
+		m.addAttribute("date", ldt);
+	}
+	
 	@GetMapping("/person")
 	public String showPersonPage() {
 		return "PersonForm";
--- src/main/webapp/views/personresult.jsp
+++ src/main/webapp/views/personresult.jsp
@@ -7,6 +7,7 @@
 <title>Insert title here</title>
 </head>
 <body>
+	<h1>${date}</h1>
 	<table>
 		<tr>
 			<td>Person ID: </td>
