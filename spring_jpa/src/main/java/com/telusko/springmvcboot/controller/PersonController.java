e8fdc2c6395ea00559cf110fc5b5e623650ff32c
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-03-06 23:21:11 +0530

    Add new object

--- spring_jpa/src/main/java/com/telusko/springmvcboot/controller/PersonController.java
+++ spring_jpa/src/main/java/com/telusko/springmvcboot/controller/PersonController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.telusko.springmvcboot.PersonRepo;
+import com.telusko.springmvcboot.model.Person;
 
 @Controller
 public class PersonController {
@@ -16,6 +18,21 @@ public class PersonController {
 	
 	@GetMapping("/getPersonJPA")
 	public String getAllPersonFROMDB(Model m) {
+		m.addAttribute("result", repo.findAll());
+		return "showPersons";
+	}
+	
+	@GetMapping("/getPerson")
+	public String getPersonFROMDB(Model m,@RequestParam int id) {
+		m.addAttribute("result", repo.getOne(id));
+		return "showPersons";
+	}
+	
+	@GetMapping("/addPerson")
+	public String addPerson(Model m,@RequestParam int id,@RequestParam String name,@RequestParam int age) {
+	
+		Person p1 = new Person(id,name,age);
+		repo.save(p1);
 		m.addAttribute("result", repo.findAll());
 		return "showPersons";
 	}
