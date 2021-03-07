f71e46b49d87109b6230589da7230c28d490fd5f
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-03-07 10:07:30 +0530

    REST API: fetch using ID

--- spring_jpa/src/main/java/com/telusko/springmvcboot/controller/PersonAPIController.java
+++ spring_jpa/src/main/java/com/telusko/springmvcboot/controller/PersonAPIController.java
@@ -2,9 +2,13 @@ package com.telusko.springmvcboot.controller;
 
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.telusko.springmvcboot.PersonRepo;
@@ -20,5 +24,12 @@ public class PersonAPIController {
 	@ResponseBody
 	public List<Person> listOfPerson(){
 		return repo.findAll();
+	}
+	
+	@GetMapping("/personAPI/{id}")
+	@ResponseBody
+	public Person getPerson(@PathVariable("id") int id) {
+		Person p = repo.findById(id).orElse(new Person());
+		return p;
 	}
 }
