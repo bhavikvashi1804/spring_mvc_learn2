4924aa00f77f663affc1f28260750b54699e776a
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-03-07 10:09:59 +0530

    @RestController: removes the need of @ResponseBody at method level

--- spring_jpa/src/main/java/com/telusko/springmvcboot/controller/PersonAPIController.java
+++ spring_jpa/src/main/java/com/telusko/springmvcboot/controller/PersonAPIController.java
@@ -2,32 +2,26 @@ package com.telusko.springmvcboot.controller;
 
 import java.util.List;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.telusko.springmvcboot.PersonRepo;
 import com.telusko.springmvcboot.model.Person;
 
-@Controller
+@RestController
 public class PersonAPIController {
 
 	@Autowired
 	PersonRepo repo;
 	
 	@GetMapping("/personAPI")
-	@ResponseBody
 	public List<Person> listOfPerson(){
 		return repo.findAll();
 	}
 	
 	@GetMapping("/personAPI/{id}")
-	@ResponseBody
 	public Person getPerson(@PathVariable("id") int id) {
 		Person p = repo.findById(id).orElse(new Person());
 		return p;
